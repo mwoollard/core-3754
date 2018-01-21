0fc065c03e979efe56520e87834323668d6ffb12
Mark Woollard <mark.woollard@urbanthings.co>
2018-01-21 17:19:30 +0000

    Initial source

---
+++ Core/Stub.swift
@@ -0,0 +1,13 @@
+//
+//  Stub.swift
+//  Core
+//
+//  Created by Mark Woollard on 21/01/2018.
+//  Copyright © 2018 ERG. All rights reserved.
+//
+
+import Foundation
+
+public func stub() -> String {
+    return "This is a stub function"
+}
